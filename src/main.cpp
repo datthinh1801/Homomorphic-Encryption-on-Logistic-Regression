6dab6188edad164a2ec2079d2d6dd474f825ec23
dat._.thinh <44528004+datthinh1801@users.noreply.github.com>
2021-05-29 20:31:09 +0700

    Use a universal encoder

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@ int main()
     // Validate parameters
     cout << "Valid: " << context.parameter_error_message() << endl;
     double scale = pow(2.0, 40);
+    CKKSEncoder encoder(context);
+    size_t slot_count = encoder.slot_count();
 
     // Generate keys
     KeyGenerator keygen(context);
@@ -21,15 +23,18 @@ int main()
     keygen.create_public_key(public_key);
     RelinKeys relin_keys;
     keygen.create_relin_keys(relin_keys);
+    GaloisKeys galois_keys;
+    keygen.create_galois_keys(galois_keys);
 
     Plaintext plain;
-    vector<double> input{0.0, 1.1, 2.2, 3.3};
-    Encode(context, input, scale, plain);
+    vector<double> input{0.0, 1.0, 2.0, 3.0};
+    Encode(encoder, input, scale, plain);
 
     Ciphertext ciphertext = Encrypt(context, public_key, scale, plain);
     Ciphertext encrypted_result = Sigmoid(context, relin_keys, scale, ciphertext);
     Plaintext recover = Decrypt(context, secret_key, encrypted_result);
-    Decode(context, recover, input);
+    Decode(encoder, recover, input);
     print_vector(input);
+
     return 0;
 }
