d968a12b90c719ad06187f6f8fceea13f00588c7
dat._.thinh <44528004+datthinh1801@users.noreply.github.com>
2021-05-30 10:03:32 +0700

    Setup to test the algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -26,15 +26,53 @@ int main()
     GaloisKeys galois_keys;
     keygen.create_galois_keys(galois_keys);
 
-    Plaintext plain;
-    vector<double> input{0.0, 1.0, 2.0, 3.0};
-    Encode(encoder, input, scale, plain);
+    // sample 1
+    Plaintext plain_input;
+    vector<double> input1{0.0, 1.0, 2.0, 3.0, 4.0, 5.0};
+    Encode(encoder, input1, scale, plain_input);
+    Ciphertext encrypted_input = Encrypt(context, public_key, scale, plain_input);
 
-    Ciphertext ciphertext = Encrypt(context, public_key, scale, plain);
-    Ciphertext encrypted_result = Sigmoid(context, relin_keys, scale, ciphertext);
-    Plaintext recover = Decrypt(context, secret_key, encrypted_result);
-    Decode(encoder, recover, input);
-    print_vector(input);
+    vector<Ciphertext> encrypted_samples;
+    encrypted_samples.push_back(encrypted_input);
+
+    // sample 2
+    vector<double> input2{0.0, 1.0, 0.0, 3.0, 1.0, 3.0};
+    Encode(encoder, input2, scale, plain_input);
+    encrypted_input = Encrypt(context, public_key, scale, plain_input);
+    encrypted_samples.push_back(encrypted_input);
+
+    // labels
+    double label = 1.0;
+    Plaintext plain_label;
+    Encode(encoder, label, scale, plain_label);
+    Ciphertext encrypted_label = Encrypt(context, public_key, scale, plain_label);
+
+    vector<Ciphertext> encrypted_labels;
+    encrypted_labels.push_back(encrypted_label);
+
+    label = 0.0;
+    Encode(encoder, label, scale, plain_label);
+    encrypted_label = Encrypt(context, public_key, scale, plain_label);
+    encrypted_labels.push_back(encrypted_label);
+
+    // weight
+    Plaintext plain_weight;
+    vector<double> weights(5, 0);
+    Encode(encoder, weights, scale, plain_weight);
+    Ciphertext encrypted_weights = Encrypt(context, public_key, scale, plain_weight);
+
+    // learning rate
+    double learning_rate = 0.5;
+    Plaintext plain_learning_rate;
+    Encode(encoder, learning_rate, scale, plain_learning_rate);
+    Ciphertext encrypted_learning_rate = Encrypt(context, public_key, scale, plain_learning_rate);
+
+    // train
+    Ciphertext encrypted_trained_weights = Train(context, relin_keys, galois_keys, scale, encrypted_samples, encrypted_labels, encrypted_weights, encrypted_learning_rate, slot_count);
+    Plaintext plain_trained_weights = Decrypt(context, secret_key, encrypted_trained_weights);
+    vector<double> trained_weights;
+    Decode(context, plain_trained_weights, trained_weights);
+    print_vector(trained_weights, 5, 7);
 
     return 0;
 }
