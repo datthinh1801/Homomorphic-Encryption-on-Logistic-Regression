cee500ec4d1c8b0ad4df7be9a56aee3dc1541b95
dat._.thinh <44528004+datthinh1801@users.noreply.github.com>
2021-06-03 17:16:44 +0700

    Enhance plain vector multiplication

--- src/plain_algorithms.hpp
+++ src/plain_algorithms.hpp
@@ -5,28 +5,22 @@ using namespace std;
 
 double PlainVectorMultiplication(const vector<double> &sample, const vector<double> &weights)
 {
-    vector<double> element_mul(sample.size(), 0);
+    double product = 0;
     for (size_t i = 0; i < sample.size(); ++i)
     {
-        element_mul[i] = sample[i] * weights[i];
+        product += sample[i] * weights[i];
     }
-
-    double sum = 0;
-    for (size_t i = 0; i < element_mul.size(); ++i)
-    {
-        sum += element_mul[i];
-    }
-    return sum;
+    return product;
 }
 
 double PlainSigmoid(const vector<double> &sample, const vector<double> &weights)
 {
-    double mul_result = PlainVectorMultiplication(sample, weights);
-    double sigmoid = 1.0 / (1 + exp(mul_result * (-1)));
+    double product = PlainVectorMultiplication(sample, weights);
+    double sigmoid = 1.0 / (1 + exp(product * (-1)));
     return sigmoid;
 }
 
-double ReportAccuracy(const vector<vector<double>> &features, const vector<double> &labels, const vector<double> &weights)
+double ComputeAccuracy(const vector<vector<double>> &features, const vector<double> &labels, const vector<double> &weights)
 {
     vector<double> result;
     size_t correct = 0;
